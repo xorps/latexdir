91381e14e5dee8a8f26ecea45b74a4c31c3f5869
xorps <758027+xorps@users.noreply.github.com>
2019-11-19 06:07:47 -0800

    update

--- src/latexdir/Barcode.fs
+++ src/latexdir/Barcode.fs
@@ -4,18 +4,21 @@ open Digits
 open Number.Types
 open Number.Values
 open Control
+open Control.Function
 
 type NDC_11 = NDC_11 of Digits<_5> * Digits<_4> * Digits<_2>
+
+let Mk_NDC_11 = curry3 NDC_11
 
 type NDC_10 =
     | NDC_4_4_2 of Digits<_4> * Digits<_4> * Digits<_2>
     | NDC_5_3_2 of Digits<_5> * Digits<_3> * Digits<_2>
     | NDC_5_4_1 of Digits<_5> * Digits<_4> * Digits<_1>
 
-let mk_NDC_11 = function
-    | NDC_4_4_2(x, y, z) -> let x = Digits.create ("0" + Digits.ToString x) _5 in Option.liftT3 NDC_11 x (Some y) (Some z)
-    | NDC_5_3_2(x, y, z) -> let y = Digits.create ("0" + Digits.ToString y) _4 in Option.liftT3 NDC_11 (Some x) y (Some z)
-    | NDC_5_4_1(x, y, z) -> let z = Digits.create ("0" + Digits.ToString z) _2 in Option.liftT3 NDC_11 (Some x) (Some y) z
+let to_NDC_11 = function
+    | NDC_4_4_2(x, y, z) -> let x = Digits.create ("0" + Digits.ToString x) _5 in Option.lift1 (skip1 Mk_NDC_11 __ y z) x
+    | NDC_5_3_2(x, y, z) -> let y = Digits.create ("0" + Digits.ToString y) _4 in Option.lift1 (skip2 Mk_NDC_11 x __ z) y
+    | NDC_5_4_1(x, y, z) -> let z = Digits.create ("0" + Digits.ToString z) _2 in Option.lift1 (skip3 Mk_NDC_11 x y __) z
 
 let possible (s: Digits<_10>) =
     let s = Digits.ToString s
@@ -34,4 +37,4 @@ let possible (s: Digits<_10>) =
         let y = Digits.create s.[5..8] _4
         let z = Digits.create s.[9..] _1
         Option.liftT3 NDC_5_4_1 x y z
-    Option.liftA3 x y z 
+    Option.lift3 (fun x y z -> [| x; y; z |]) x y z
--- src/latexdir/Control.fs
+++ src/latexdir/Control.fs
@@ -1,16 +1,24 @@
 ﻿module Control
 
-module Option =
-    let liftT3 f a b c =
-        a |> Option.bind (fun x ->
-        b |> Option.bind (fun y ->
-        c |> Option.map (fun z ->
-            f (x, y, z)
-        )))
+module Function =
+    type Skip() = class end
+    let __ = Skip()
+    let curry2 f a b = f (a, b)
+    let curry3 f a b c = f (a, b, c)
+    let skip1 f (_: Skip) b c = fun a -> f a b c
+    let skip2 f a (_: Skip) c = fun b -> f a b c
+    let skip3 f a b (_: Skip) = fun c -> f a b c
 
-    let liftA3 a b c =
+module Option =
+    let lift1 f a =
+        a |> Option.map (fun a -> f a)
+
+    let lift3 f a b c =
         a |> Option.bind (fun x ->
         b |> Option.bind (fun y ->
         c |> Option.map (fun z ->
-            [| x; y; z |]
+            f x y z
         )))
+        
+    let liftT3 f = lift3 (fun x y z -> f (x, y, z))
+
